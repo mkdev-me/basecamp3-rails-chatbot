9c5a0a1c3228506e1ff83d8bb2547753127c71ea
Klim Agarkov <glock@mail.ru>
2017-03-22 13:17:38 +0300

    Fixed development.rb
    - Removed extra empty string,
    - Added comment to Web Console config string.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
+  # Add your IP's to whitelist
   config.web_console.whitelisted_ips = '54.240.230.0/16'
 
 end
