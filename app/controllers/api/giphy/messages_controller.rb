f750b1c218fb539d2556057cef577f647e8b6ffe
Klim Agarkov <jewelgatorstv@gmail.com>
2017-04-02 16:12:48 +0300

    Fix Giphy controller

--- app/controllers/api/giphy/messages_controller.rb
+++ app/controllers/api/giphy/messages_controller.rb
@@ -4,9 +4,9 @@ class Api::Giphy::MessagesController < ApplicationController
   def create
     giphy_url = Rails.configuration.service['giphy_url']
     giphy_parsed = JSON.parse HTTParty.get(giphy_url).body
-    giphy_response_status = giphy_parsed['meta']['status']
+    giphy_response_status = giphy_parsed['meta']['msg']
   
-    if giphy_response_status == 200
+    if giphy_response_status == 'OK'
       message = giphy_parsed['data']['fixed_width_downsampled_url']    
     else
       message = "<strong>There's a problem with Gliphy API:</strong> response status = #{giphy_response_status}"
@@ -14,6 +14,5 @@ class Api::Giphy::MessagesController < ApplicationController
 
     # send message to basecamp
     helpers.send_message(message)
-
   end
 end
