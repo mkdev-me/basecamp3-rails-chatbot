048c029479d1e8cf4e826e08f3e788322a8f49df
Klim Agarkov <jewelgatorstv@gmail.com>
2017-04-05 15:01:17 +0300

    Fix Aws initializer

--- config/initializers/aws_sdk.rb
+++ config/initializers/aws_sdk.rb
@@ -1,8 +1,9 @@
+# aws_secret_file - path to *.yml which contains with AWS_ACCESS_KEY_ID and AWS_SECRET_KEY_ID
 aws_secret_file = Rails.configuration.service['aws_secret_file']
 creds = YAML.load(File.read(aws_secret_file))
-  aws_creds = Aws::Credentials.new(
-    creds['aws_access_key'],
-    creds['aws_secret_key']
-  )
-  # Configure and create an AWS_SNS client
+aws_creds = Aws::Credentials.new(
+ creds['aws_access_key'],
+ creds['aws_secret_key']
+)
+# Configure an AWS_SNS client
 Aws.config.update(region: creds['aws_region'], credentials: aws_creds)
