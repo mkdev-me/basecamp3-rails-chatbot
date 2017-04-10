796e41f24ff668b66b68cf5bbdf2c1fc09b5a401
Klim Agarkov <jewelgatorstv@gmail.com>
2017-04-10 13:06:17 +0300

    Amazon SNS subscription by using Instance profile

--- config/initializers/aws_sdk.rb
+++ config/initializers/aws_sdk.rb
@@ -1,9 +1,18 @@
-# aws_secret_file: path to *.yml with AWS_ACCESS_KEY_ID and AWS_SECRET_KEY_ID
+# aws_secret_file: path to *.yml file with AWS_ACCESS_KEY_ID and AWS_SECRET_KEY_ID if exist
 aws_secret_file = Rails.configuration.service['aws_secret_file']
-creds = YAML.load(File.read(aws_secret_file))
-aws_creds = Aws::Credentials.new(
- creds['aws_access_key'],
- creds['aws_secret_key']
-)
-# Configure an AWS_SNS client
-Aws.config.update(region: creds['aws_region'], credentials: aws_creds)
+aws_region = Rails.configuration.service['aws_secret_file']
+
+if aws_secret_file.nil? 
+	credentials = Aws::InstanceProfileCredentials.new
+	Aws.config.update({
+   region: aws_region,
+   credentials: credentials
+  })
+else
+	creds = YAML.load(File.read(aws_secret_file))
+	aws_creds = Aws::Credentials.new(
+	 creds['aws_access_key'],
+	 creds['aws_secret_key']
+	)
+	Aws.config.update(region: creds['aws_region'], credentials: aws_creds)
+end
