149f2d77248ce37a5c6992be499d6094dbaba538
Klim Agarkov <glock@mail.ru>
2017-04-21 19:06:09 +0400

    Update aws_sdk.rb

--- config/initializers/aws_sdk.rb
+++ config/initializers/aws_sdk.rb
@@ -6,6 +6,6 @@ if aws_secret_file?
   aws_creds = Aws::Credentials.new(
    creds['aws_access_key'],
    creds['aws_secret_key']
-  )
+   )
   Aws.config.update(region: creds['aws_region'], credentials: aws_creds)
 end
