90f0a29432c4252cd057be2f17cb0a9c1fdc2fb2
Klim Agarkov <glock@mail.ru>
2017-04-28 21:57:56 +0400

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token, if: :json_request?
-  
+
   private
 
   def json_request?
