db86b21f014a81da4e4defa6952288eca26e79f4
Klim Agarkov <glock@mail.ru>
2017-04-28 22:02:02 +0400

    Update messages_controller.rb

--- app/controllers/api/giphy/messages_controller.rb
+++ app/controllers/api/giphy/messages_controller.rb
@@ -1,5 +1,4 @@
 class Api::Giphy::MessagesController < ApplicationController
-
   def create
     image = Giphy.random(command_params[:command]).image_original_url.to_s
     Chatbot.send_message(command_params[:callback_url], "<img src='#{image}' />")
